35bb4a4a15f5fa9a0907cf307b80c3c05692df7a
Damian <AdrianAndradeChristian@outlook.com>
2023-02-18 17:51:34 -0500

    Implementing the dbContext

---
+++ MyApp/MyAPI/Extensions/ServicesExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPersistence;
+
+namespace MyAPI.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void AddJWT(this IServiceCollection services, IConfiguration configuration)
+        { }
+
+        public static void AddOracle(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["ConnectionString:Oracle"];
+
+            services.AddDbContext<MyDbContext>(opt => opt.UseOracle(connectionString));
+        }
+    }
+}
